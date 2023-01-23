a95fe81a94e4c91783ef7c8d3cb8117e66d40cfd
deyncoder <delacruzdaniellemarie@yahoo.com>
2023-01-23 23:46:58 +0800

    Initial Commit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-set root=D:\D Files\Programs\PyCharmProjects\hybridframework-python-selenium
+set root=D:\DFiles\Programs\PyCharmProjects\hybridframework-python-selenium
 cd /D %root%
 pytest -s -v -m "sanity" --html=./Reports/report_chrome.html testCases/ --browser chrome
 pytest -s -v -m "sanity" --html=./Reports/report_edge.html testCases/ --browser edge
